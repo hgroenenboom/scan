f41fdad367dd0ddda6e36cf2ba9afaa3b0aef352
Milan <vandermeer.milan@gmail.com>
2022-03-11 15:20:14 +0100

    crash

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  scan
+//
+//  Created by Milan van der Meer on 11/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+        
+    int* pointer = nullptr;
+    
+    if (*pointer > 0)
+    {
+        std::cout << "hello" << std::endl;
+    }
+    
+    return 0;
+}
